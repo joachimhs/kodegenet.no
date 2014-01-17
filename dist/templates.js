00e2ee4d673ff01ca07d907db7ffaf4f5e6c56a9
Joachim Haagen Skeie <joachim@haagen-software.no>
2014-01-17 14:02:49 +0100

    Viser innholdet til samtlige oppgaver når man ser på et kapittel

--- dist/templates.js
+++ dist/templates.js
@@ -86,11 +86,22 @@ function program7(depth0,data) {
 Ember.TEMPLATES["course/chapter"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
 this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
-  var buffer = '', helper, options, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
+  var buffer = '', stack1, helper, options, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, self=this;
 
+function program1(depth0,data) {
+  
+  var buffer = '', helper, options;
+  data.buffer.push("\n        ");
+  data.buffer.push(escapeExpression((helper = helpers.markdown || (depth0 && depth0.markdown),options={hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data},helper ? helper.call(depth0, "oppgave.content", options) : helperMissing.call(depth0, "markdown", "oppgave.content", options))));
+  data.buffer.push("\n    ");
+  return buffer;
+  }
 
   data.buffer.push("<div class=\"well chapter\">\n    ");
   data.buffer.push(escapeExpression((helper = helpers.markdown || (depth0 && depth0.markdown),options={hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data},helper ? helper.call(depth0, "content.content", options) : helperMissing.call(depth0, "markdown", "content.content", options))));
+  data.buffer.push("\n\n    ");
+  stack1 = helpers.each.call(depth0, "oppgave", "in", "oppgaver", {hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0,depth0,depth0],types:["ID","ID","ID"],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
   data.buffer.push("\n</div>");
   return buffer;
   
